40fe41d9af56938cebf148b232f82ea763d43a03
Monica Garcia <monicairenegarcia@gmail.com>
2024-03-02 18:23:44 -0700

    make initial GET request

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,7 +1,13 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function AddressBook() {
+    useEffect(() => {
+        fetch("http://localhost:3000/contacts")
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(error => console.error(error));
+    }, [])
     return (
         <div>
             <h1>Address Book</h1>
