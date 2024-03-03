ee09e9e1641057f8c175d260e86d0164d0143359
Monica Garcia <monicairenegarcia@gmail.com>
2024-03-02 18:19:55 -0700

    create AddressBook component

---
+++ src/AddressBook.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { useState } from "react";
+
+function AddressBook() {
+    return (
+        <div>
+            <h1>Address Book</h1>
+            <ul>
+                <li>
+                    <input type="text" />
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default AddressBook;
