0a4a8793bb1aaf3ba9ba148acf3af249d691795f
Monica Garcia <monicairenegarcia@gmail.com>
2024-03-09 18:32:29 -0700

    create family group

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './NavBar';
 import AddressBook from './AddressBook';
+import Groups from './Groups';
 
 function App() {
+  
   return (
     <div className="App">
       <header>
         <NavBar />
       </header>
       <AddressBook />
+      <Groups />
     </div>
   );
 }
--- src/Groups.js
+++ src/Groups.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import NavBar from './NavBar';
+import { useEffect, useState} from'react';
+import ContactCard from './ContactCard';
 
 function Groups() {
+    const [groupContacts, setGroupContacts] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:3000/contacts")
+        .then(res => res.json())
+        .then(data => setGroupContacts(data))
+        .catch(error => console.error(error));
+    }, [])
+
     return (
         <div>
-            <h1>Groups</h1>
-            <div></div>
+            <header>
+                <NavBar />
+            </header>
+            <main>
+                <h1>Groups</h1>
+                <h2>Family</h2>
+                <div>
+                    {groupContacts.filter(contact => contact.group === "family")
+                    .map(contact => {
+                        return (
+                            <ContactCard name={contact.name} phone={contact.phone} nickname={contact.nickname} favorite={contact.favorite} key={contact.id} group={contact.group}/>
+                        )
+                    })}
+                </div>
+            </main>
         </div>
     )
 }
