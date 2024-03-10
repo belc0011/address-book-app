0c9f7d33d6745c6f568626392758d22fc6dddeb2
Monica Garcia <monicairenegarcia@gmail.com>
2024-03-09 17:41:36 -0700

    add NavBar to AddressBook

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import ContactCard from "./ContactCard.js";
+import NavBar from "./NavBar.js";
 
 function AddressBook() {
     const [contacts, setContacts] = useState([]);
@@ -12,12 +13,17 @@ function AddressBook() {
     }, [])
     return (
         <div>
-            <h1>Address Book</h1>
-            {contacts.map(contact => {
-                return (
-                    <ContactCard name={contact.name} phone={contact.phone} nickname={contact.nickname} favorite={contact.favorite} key={contact.id} group={contact.group}/>
-                )
-            })}
+            <header>
+                <NavBar />
+            </header>
+            <main>
+                <h1>Address Book</h1>
+                {contacts.map(contact => {
+                    return (
+                        <ContactCard name={contact.name} phone={contact.phone} nickname={contact.nickname} favorite={contact.favorite} key={contact.id} group={contact.group}/>
+                    )
+                })}
+            </main>
         </div>
     )
 }
