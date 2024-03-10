38db80591d6731d93087fc7a990b506e87f8343f
Monica Garcia <monicairenegarcia@gmail.com>
2024-03-09 19:20:54 -0700

    make changes to form

--- src/Form.js
+++ src/Form.js
@@ -1,18 +1,45 @@
 import React from 'react';
+import NavBar from './NavBar';
 
 function Form() {
     return (
         <div>
-            <form>
-                <input type="text" placeholder="Name" />
-                <input type="text" placeholder="Phone" />
-                <input type="text" placeholder="Nickname" />
-                <input type="text" placeholder="Group" />
-                <input type="dropdown" placeholder="Favorite?" />
-                <option value="true">Yes</option>
-                <option value="false">No</option>
-                <button type="submit">Submit</button>
-            </form>
+            <header>
+                <NavBar />
+            </header>
+            <main>
+                <form>
+                    <label for="name">Name</label>
+                    <div>
+                        <input type="text" placeholder="New contact name" id="name" />
+                    </div>
+                    <label for="phone">Phone</label>
+                    <div>
+                        <input type="text" placeholder="New contact phone" />
+                    </div>
+                    <label for="nickname">Nickname</label>
+                    <div>
+                        <input type="text" placeholder="Nickname" />
+                    </div>
+                    <label for="group">Group</label>
+                    <div>
+                        <select type="dropdown" id="group">
+                            <option value="friends">Friends</option>
+                            <option value="family">Family</option>
+                            <option value="in-laws">In-Laws</option>
+                            <option value="other">Other</option>
+                        </select>
+                    </div>
+                    <label for="favorite">Favorite?</label>
+                    <div>
+                    <select type="dropdown" id="favorite">
+                        <option value="true">Yes</option>
+                        <option value="false">No</option>
+                    </select>
+                    </div>
+                    <button type="submit">Submit</button>
+                </form>
+            </main>
         </div>
     )
 }
